50dac368f818184b35b30f5e44146b330e21384e
kmtdlah <99109389+kmtdlah@users.noreply.github.com>
2022-03-08 06:27:55 +0000

    Add files via upload

--- handler.js
+++ handler.js
@@ -26,225 +26,61 @@ module.exports = {
         let user = global.db.data.users[m.sender]
         if (typeof user !== 'object') global.db.data.users[m.sender] = {}
         if (user) {
-            if (!isNumber(user.healt)) user.healt = 0
-            if (!isNumber(user.level)) user.level = 0
-            if (!isNumber(user.exp)) user.exp = 0
-            if (!isNumber(user.title)) user.title = ''
-            if (!isNumber(user.limit)) user.limit = 50
-            if (!isNumber(user.lastclaim)) user.lastclaim = 0
-            if (!isNumber(user.money)) user.money = 0
-            
-            if (!isNumber(user.diamond)) user.diamond = 0
-            if (!isNumber(user.iron)) user.iron = 0
-
-            if (!isNumber(user.common)) user.common = 0
-            if (!isNumber(user.uncommon)) user.uncommon = 0
-            if (!isNumber(user.mythic)) user.mythic = 0
-            if (!isNumber(user.legendary)) user.legendary = 0
-            if (!isNumber(user.pet)) user.pet = 0
-        
-            if (!isNumber(user.potion)) user.potion = 0
-            if (!isNumber(user.sampah)) user.sampah = 0
-            if (!isNumber(user.armor)) user.armor = 0
-            
-            if (!isNumber(user.kucing)) user.kucing = 0
-            if (!isNumber(user.kucinglastclaim)) user.kucinglastclaim = 0
-            if (!isNumber(user.kuda)) user.kuda = 0
-            if (!isNumber(user.kudalastclaim)) user.kudalastclaim = 0
-            if (!isNumber(user.rubah)) user.rubah = 0
-            if (!isNumber(user.rubahlastclaim)) user.rubahlastclaim = 0
-            if (!isNumber(user.anjing)) user.anjing = 0
-            if (!isNumber(user.anjinglastclaim)) user.anjinglastclaim = 0
-
-            if (!'banned' in user) user.banned = false
-            if (!isNumber(user.warn)) user.warn = 0
-
-            if (!isNumber(user.afk)) user.afk = -1
-            if (!'afkReason' in user) user.afkReason = ''
-        
-            if (!isNumber(user.anakkucing)) user.anakkucing = 0
-            if (!isNumber(user.anakkuda)) user.anakkuda = 0
-            if (!isNumber(user.anakrubah)) user.anakrubah = 0
-            if (!isNumber(user.anakanjing)) user.anakanjing = 0
-            if (!isNumber(user.makananpet)) user.makananpet = 0
-
-            if (!isNumber(user.antispam)) user.antispam = 0
-            if (!isNumber(user.antispamlastclaim)) user.antispamlastclaim = 0
-
-            if (!isNumber(user.kayu)) user.kayu = 0
-            if (!isNumber(user.batu)) user.batu = 0
-            if (!isNumber(user.string)) user.string = 0
-            if (!isNumber(user.sword)) user.sword = 0
-            if (!isNumber(user.sworddurability)) user.sworddurability = 0
-            if (!isNumber(user.pickaxe)) user.pickaxe = 0
-            if (!isNumber(user.pickaxedurability)) user.pickaxedurability = 0
-            if (!isNumber(user.fishingrod)) user.fishingrod = 0
-            if (!isNumber(user.fishingroddurability)) user.fishingroddurability = 0
-            
-            if (!isNumber(user.paus)) user.paus = 0
-            if (!isNumber(user.kepiting)) user.kepiting = 0
-            if (!isNumber(user.gurita)) user.gurita = 0
-            if (!isNumber(user.cumi)) user.cumi= 0
-            if (!isNumber(user.buntal)) user.buntal = 0
-            if (!isNumber(user.dory)) user.dory = 0
-            if (!isNumber(user.lumba)) user.lumba = 0
-            if (!isNumber(user.lobster)) user.lobster = 0
-            if (!isNumber(user.hiu)) user.hiu = 0
-            if (!isNumber(user.udang)) user.udang = 0
-            if (!isNumber(user.ikan)) user.ikan = 0
-            if (!isNumber(user.orca)) user.orca = 0
-
-            if (!isNumber(user.lastadventure)) user.lastadventure = 0
-            if (!isNumber(user.lastfishing)) user.lastfishing = 0
-            if (!isNumber(user.lastdungeon)) user.lastdungeon = 0
-            if (!isNumber(user.lastduel)) user.lastduel = 0
-            if (!isNumber(user.lastmining)) user.lastmining = 0
-            if (!isNumber(user.lasthunt)) user.lasthunt = 0
-            if (!isNumber(user.lastweekly)) user.lastweekly = 0
-            if (!isNumber(user.lastmonthly)) user.lastmontly = 0
-            if (!('registered' in user)) user.registered = false
-            if (!user.registered) {
-                if (!('name' in user)) user.name = this.getName(m.sender)
-                if (!isNumber(user.age)) user.age = -1
-                if (!isNumber(user.serial)) user.serial
-                if (!isNumber(user.regTime)) user.regTime = -1
-            }
-            if (!('autolevelup' in user)) user.autolevelup = false
-            if (!('lastIstigfar' in user)) user.lastIstigfar = true
+          if (!isNumber(user.exp)) user.exp = 0
+          if (!isNumber(user.limit)) user.limit = 10
+          if (!isNumber(user.lastclaim)) user.lastclaim = 0
+          if (!('registered' in user)) user.registered = false
+          if (!user.registered) {
+            if (!('name' in user)) user.name = this.getName(m.sender)
+            if (!isNumber(user.age)) user.age = -1
+            if (!isNumber(user.regTime)) user.regTime = -1
+          }
+          if (!isNumber(user.afk)) user.afk = -1
+          if (!('afkReason' in user)) user.afkReason = ''
+          if (!('banned' in user)) user.banned = false
+          if (!isNumber(user.level)) user.level = 0
+          if (!user.role) user.role = 'Beginner'
+          if (!('autolevelup' in user)) user.autolevelup = false
         } else global.db.data.users[m.sender] = {
-            healt: 100,
-            level: 0,
-            title: '',
-            exp: 0,
-            limit: 50,
-            lastclaim: 0,
-            money: 0,
-            diamond: 0,
-            iron: 0,
-            common: 0,
-            uncommon: 0,
-            mythic: 0,
-            legendary: 0,
-            pet: 0,
-            potion: 0,
-            sampah: 0,
-            armor: 0,
-            kucing: 0,
-            as: 0,
-            paus: 0,
-            kepiting: 0,
-            gurita: 0,
-            cumi: 0,
-            buntal: 0,
-            dory: 0,
-            lumba: 0,
-            lobster: 0,
-            hiu: 0,
-            udang: 0,
-            ikan: 0,
-            orca: 0,
-            kucinglastclaim: 0,
-            kuda: 0,
-            kudalastclaim: 0,
-            rubah: 0,
-            rubahlastclaim: 0,
-            anjing: 0,
-            anjinglastclaim: 0,
-            banned: false,
-            warn: 0,
-            afk: -1,
-            afkReason: '',
-            anakkucing: 0,
-            anakkuda: 0,
-            anakrubah: 0,
-            anakanjing: 0,
-            makananpet: 0,
-            antispam: 0,
-            antispamlastclaim: 0,
-            kayu: 0,
-            batu: 0,
-            string: 0,
-            sword: 0,
-            sworddurability: 0,
-            pickaxe: 0,
-            pickaxedurability: 0,
-            fishingrod: 0,
-            fishingroddurability: 0,
-            lastadventure: 0,
-            lastfishing: 0,
-            lastdungeon: 0,
-            lastduel: 0,
-            lastmining: 0,
-            lasthunt: 0,
-            lastweekly: 0,
-            lastmonthly: 0,
-            registered: false,
-            name: this.getName(m.sender),
-            age: -1,
-            serial: serial,
-            regTime: -1,
-            autolevelup: false,
-            lastIstigfar: 0,
+          exp: 0,
+          limit: 20,
+          lastclaim: 0,
+          registered: false,
+          name: this.getName(m.sender),
+          age: -1,
+          regTime: -1,
+          afk: -1,
+          afkReason: '',
+          banned: false,
+          level: 0,
+          role: 'Beginner',
+          autolevelup: false,
         }
 
         let chat = global.db.data.chats[m.chat]
         if (typeof chat !== 'object') global.db.data.chats[m.chat] = {}
         if (chat) {
           if (!('isBanned' in chat)) chat.isBanned = false
-          if (!('welcome' in chat)) chat.welcome = true
+          if (!('welcome' in chat)) chat.welcome = false
           if (!('detect' in chat)) chat.detect = false
           if (!('sWelcome' in chat)) chat.sWelcome = ''
           if (!('sBye' in chat)) chat.sBye = ''
           if (!('sPromote' in chat)) chat.sPromote = ''
           if (!('sDemote' in chat)) chat.sDemote = ''
-          if (!('descUpdate' in chat)) chat.descUpdate = true
-          if (!('delete' in chat)) chat.delete = false
-          if (!('antiBadword' in chat)) chat.antiBadword = true
-          if (!('rpg' in chat)) chat.delete = true
-          if (!('nsfw' in chat)) chat.delete = false
+          if (!('delete' in chat)) chat.delete = true
           if (!('antiLink' in chat)) chat.antiLink = false
-          if (!('viewonce' in chat)) chat.viewonce = true
+          if (!('viewonce' in chat)) chat.viewonce = false
         } else global.db.data.chats[m.chat] = {
           isBanned: false,
-          welcome: true,
+          welcome: false,
           detect: false,
           sWelcome: '',
           sBye: '',
           sPromote: '',
           sDemote: '',
-          descUpdate: true,
-          delete: false,
-          rpg: true,
-          nsfw: false,
-          antiBadword: true,
+          delete: true,
           antiLink: false,
-          viewonce: true,
-        }
-        
-                let settings = global.db.data.settings[this.user.jid]
-        if (typeof settings !== 'object') global.db.data.settings[this.user.jid] = {}
-        if (settings) {
-          if (!'anon' in settings) settings.anon = true
-          if (!'anticall' in settings) settings.anticall = true
-          if (!'antispam' in settings) settings.antispam = true
-          if (!'antitroli' in settings) settings.antitroli = false
-          if (!'backup' in settings) settings.backup = true
-          if (!isNumber(settings.backupDB)) settings.backupDB = 0
-          if (!'groupOnly' in settings) settings.groupOnly = false
-          if (!'jadibot' in settings) settings.groupOnly = false
-          if (!'nsfw' in settings) settings.nsfw = true
-          if (!isNumber(settings.status)) settings.status = 0
-        } else global.db.data.settings[this.user.jid] = {
-          anon: true,
-          anticall: true,
-          antispam: true,
-          antitroli: false,
-          backup: true,
-          backupDB: 0,
-          groupOnly: false,
-          jadibot: false,
-          onsfw: true,
-          status: 0,
+          viewonce: false,
         }
       } catch (e) {
         console.error(e)
@@ -269,10 +105,6 @@ module.exports = {
         }
       }
       if (m.isBaileys) return
-      if (m.isBaileys) return
-      if (m.chat.endsWith('broadcast')) return // Supaya tidak merespon di status
-      let blockList = conn.blocklist.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-      if (blockList.includes(m.sender)) return // Pengguna yang diblokir tidak bisa menggunakan bot
       m.exp += Math.ceil(Math.random() * 10)
 
       let usedPrefix
@@ -288,13 +120,11 @@ module.exports = {
       let bot = m.isGroup ? participants.find(u => u.jid == this.user.jid) : {} // Your Data
       let isAdmin = user.isAdmin || user.isSuperAdmin || false // Is User Admin?
       let isBotAdmin = bot.isAdmin || bot.isSuperAdmin || false // Are you Admin?
-      let isBlocked = this.blocklist.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != this.user.jid).includes(m.sender) // Apakah user diblokir?
       for (let name in global.plugins) {
         let plugin = global.plugins[name]
         if (!plugin) continue
         if (plugin.disabled) continue
-        //if (!opts['restrict']) if (plugin.tags && plugin.tags.includes('admin')) continue
-        // if (!opts['nsfw']) if (plugin.tags && plugin.tags.includes('hentai', 'bokep')) continue
+        if (!opts['restrict']) if (plugin.tags && plugin.tags.includes('admin')) continue
         const str2Regex = str => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
         let _prefix = plugin.customPrefix ? plugin.customPrefix : conn.prefix ? conn.prefix : global.prefix
         let match = (_prefix instanceof RegExp ? // RegExp Mode?
@@ -323,7 +153,6 @@ module.exports = {
           isBotAdmin,
           isPrems,
           chatUpdate,
-          isBlocked,
         })) continue
         if (typeof plugin !== 'function') continue
         if ((usedPrefix = (match[0] || '')[0])) {
@@ -397,11 +226,11 @@ module.exports = {
           if (xp > 200) m.reply('Ngecit -_-') // Hehehe
           else m.exp += xp
           if (!isPrems && plugin.limit && global.db.data.users[m.sender].limit < plugin.limit * 1) {
-            this.reply(m.chat, `Limit anda habis, silahkan beli melalui *${usedPrefix}buy*`, m)
-            continue // Limit habis
+            this.reply(m.chat, `Your limit is up, please buy via *${usedPrefix}buy*`, m)
+            continue // Limit run out
           }
           if (plugin.level > _user.level) {
-            this.reply(m.chat, `diperlukan level ${plugin.level} untuk menggunakan perintah ini. Level kamu ${_user.level}`, m)
+            this.reply(m.chat, `level required ${plugin.level} to use this command. Your level ${_user.level}`, m)
             continue // If the level has not been reached
           }
           let extra = {
@@ -423,7 +252,6 @@ module.exports = {
             isBotAdmin,
             isPrems,
             chatUpdate,
-            isBlocked,
           }
           try {
             await plugin.call(this, m, extra)
@@ -447,7 +275,7 @@ module.exports = {
                 console.error(e)
               }
             }
-            if (m.limit) m.reply(+ m.limit + ' Limit terpakai')
+            if (m.limit) m.reply(+ m.limit + ' Limit used')
           }
           break
         }
@@ -491,7 +319,6 @@ module.exports = {
         console.log(m, m.quoted, e)
       }
       if (opts['autoread']) await this.chatRead(m.chat).catch(() => { })
-    //this.chatRead(m.chat).catch(() => { })
     }
   },
   async participantsUpdate({ jid, participants, action }) {
@@ -535,50 +362,46 @@ module.exports = {
   async delete(m) {
     if (m.key.fromMe) return
     let chat = global.db.data.chats[m.key.remoteJid]
-    if (chat.delete) {
+    if (chat.delete) return
     await this.reply(m.key.remoteJid, `
-Terdeteksi @${m.participant.split`@`[0]} telah menghapus pesan
+Detected @${m.participant.split`@`[0]} deleted message
 
-Untuk mematikan fitur ini, ketik
-*.disable delete*
+To turn off this feature, type
+*.enable delete*
 `.trim(), m.message, {
       contextInfo: {
         mentionedJid: [m.participant]
       }
     })
     this.copyNForward(m.key.remoteJid, m.message).catch(e => console.log(e, m))
-  }
-},
+  },
   async onCall(json) {
     let { from } = json[2][0][1]
     let users = global.db.data.users
     let user = users[from] || {}
     if (user.whitelist) return
-    if (!db.data.settings.anticall) return
     switch (this.callWhitelistMode) {
       case 'mycontact':
         if (from in this.contacts && 'short' in this.contacts[from])
           return
         break
     }
-    await this.sendMessage(from, 'Maaf, karena anda menelfon bot. anda diblokir otomatis', MessageType.extendedText)
+    await this.sendMessage(from, 'Sorry, because you called the bot. you are automatically blocked', MessageType.extendedText)
     await this.blockUser(from, 'add')
   }
 }
 
 global.dfail = (type, m, conn) => {
-	let name = conn.getName(m.sender)
   let msg = {
-    rowner: `Khusus OWNER OM`,
-    owner: `Khusus OWNER OM`,
-    mods: `Khusus ADMIN BOT`,
-    premium: 'Khusus Premium!',
-    group: 'Di Grup aja!',
-    private: 'Pribadi Aja OM',
-    admin: 'Khusus *Admin* grup!',
-    nsfw: `Perintah ini hanya bisa diaktifkan oleh @${global.kontak[0].split`@`[0]}`,
-    botAdmin: 'Jadikan Bot sebagai admin untuk menggunakan perintah ini\n\nDenger ya dekkk!!!\nApakah orang yang tidak menjadi admin bisa menambahkan member???!!!!!',
-    unreg: `Daftar Dulu Om\nKetik .register `
+    rowner: 'This command can only be used by *OWNER* !',
+    owner: 'This command can only be used by *Bot Owner* !',
+    mods: 'This command can only be used by *Moderator* !',
+    premium: 'This command is only for *Premium Members* !',
+    group: 'This command can only be used in groups !',
+    private: 'This command can only be used in Private Chat !',
+    admin: 'This command is only for *Group Admin* !',
+    botAdmin: 'Make bot as *Admin* to use this command !',
+    unreg: 'Please register to use this feature by typing:\n\n*#register name.age*\n\nExample: *#register David.21*'
   }[type]
   if (msg) return m.reply(msg)
 }
